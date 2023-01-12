b4d13324f5bad2b21ffcd989002475c65c4063c6
Emetepe <martape@gmail.com>
2023-01-12 06:07:41 +0100

    Final 01.JS_Basic_Algorithms

--- 01.js_basic_algorithms/app.js
+++ 01.js_basic_algorithms/app.js
@@ -96,21 +96,27 @@ console.log(valor)
 
 // 4.2 Cambia el primer elemento de avengers a "IRONMAN"
 const avengers2 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers2[0] = "IRONMAN";
+console.log(avengers2)
 
 // 4.3 console numero de elementos en el array usando la propiedad correcta de Array.
 const avengers3 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
-
+items = avengers3.length;
+console.log(items)
 // 4.4 Añade 2 elementos al array: "Morty" y "Summer". 
 // Muestra en consola el último personaje del array
 const rickAndMortyCharacters1 = ["Rick", "Beth", "Jerry"];
 
-//TODAVIA NO
 // 4.5 Elimina el último elemento del array y muestra el primero y el último por consola.
 const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2[0]);
+console.log(rickAndMortyCharacters2[rickAndMortyCharacters2.length-1]);
 
-// TODAVIA NO
 // 4.6 Elimina el segundo elemento del array y muestra el array por consola.
 const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1,1);
+console.log(rickAndMortyCharacters3)
 
 // Iteración #5: Condicionales
 
@@ -120,42 +126,59 @@ const number3 = 2;
 
 // ejemplo
 if(number1 === 10){
-    console.log('number1 es estrictamente igual a 10')
+    console.log(`${number1} es estrictamente igual a 10`)
 }
 
-if (/* COMPLETAR */) {
-  console.log("number2 dividido entre number1 es igual a 2");
+if (number2/number1 == 2) {
+  console.log(`${number2} dividido entre ${number1} es igual a 2`);
 }
 
-if (/* COMPLETAR */) {
-  console.log("number1 es estrictamente distinto a number2");
+if (number1!==number2) {
+  console.log(`${number1} es estrictamente distinto a ${number2}`);
 }
 
-if (/* COMPLETAR */) {
-  console.log("number3 es distinto number1");
+if (number3 != number1) {
+  console.log(`${number3} es distinto ${number1}`);
 }
 
-if (/* COMPLETAR */) {
-  console.log("number3 por 5 es igual a number1");
+if ((number3*5)==number1) {
+  console.log(`${number3} por 5 es igual a ${number1}`);
 }
 
-if (/* COMPLETAR */) {
-  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+if (((number3*5)==(number1))&&((number1*2)==number2)) {
+  console.log(`${number3} por 5 es igual a ${number1} Y ${number1} por 2 es igual a ${number2}`);
 }
 
-if (/* COMPLETAR */) {
-  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+if ((number2/2)==(number1 )) {
+  console.log(`${number2} entre 2 es igual a ${number1} O ${number1} entre 5 es igual a ${number3}`);
 }
 
 
 // Iteración #6: Bucles
 
 // 6.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+loop = [0,1,2,3,4,5,6,7,8,9]
+
+for (let i=0; i<loop.length; i++){
+  console.log(i)
+}
 
 // 6.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 //cuando el resto del numero dividido entre 2 sea 0.
+loop = [0,1,2,3,4,5,6,7,8,9]
+
+for (let i=0; i<loop.length; i++){
+  if (i%2 == 0) {
+    console.log(i)
+  }
+}
 
 // 6.3 Crea un bucle para conseguir dormir contando ovejas. 
 // Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
 // Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
 // y cambia el mensaje en la décima vuelta a 'Dormido!'.
+for (let i=0; i<10; i++){
+  if (i === 9) {
+    console.log("Dormido!")
+  } else console.log("Intentando dormir")
+}
